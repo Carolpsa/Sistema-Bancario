d20bdcf80d5357d875d8b83c13bd3816f5644f5a
Carol <carolpsa.unesp@gmail.com>
2024-05-28 19:17:07 -0300

    Criacao de menu de cadastro, ajuste da verificacao do cpf

--- Banco.py
+++ Banco.py
@@ -1,27 +1,41 @@
 LIMITE_VALOR_SAQUE = 500.00
 LIMITE_QTD_SAQUE = 3
-Saldo = 0.00
+saldo = 0.00
 contador = 0
 contador_conta = 0
-Extrato = ""
+extrato = ""
 lista_clientes=[]
-menu = """ 
-    ..........MENU..........
+lista_contas=[]
+menu_operacoes = """ 
+    ..........MENU.OPERAÇÕES..........
     
     Digite S para Sacar
     Digite D para Depósito
     Digite E para Extrato
     Digite Q para Sair
     
-    .........................
+    .................................
     
     -> """
 
-def saque(Saldo, valor_saque, Extrato, numero_limite_saques, valor_limite_saque, contador):
+menu_cadastro = """ 
+    ..........Menu.de.Cadastro..........
+    
+    Digite C para Cadastrar Cliente
+    Digite O para Cadastrar Conta
+    Digite IC para Visualizar a Lista de Clientes
+    Digite IO para Visualizar a Lista de Contas
+    Digite M para Menu de Operacoes
+    
+    ...................................
+    
+    -> """
+
+def saque(saldo, valor_saque, extrato, numero_limite_saques, valor_limite_saque, contador):
     if(valor_saque < 0):
         print("Valor de saque inválido!")
     else: 
-        if(valor_saque > Saldo):
+        if(valor_saque > saldo):
             print("Saldo insuficiente")
         elif(valor_saque > valor_limite_saque):
             print("Você atingiu o valor limite de saque diário!")
@@ -29,88 +43,110 @@ def saque(Saldo, valor_saque, Extrato, numero_limite_saques, valor_limite_saque,
             print("Você atingiu a quantidade limite de saques diários!")
         else:
             contador += 1
-            saldo_final_saque = Saldo - valor_saque
-            Saldo = saldo_final_saque
-            Extrato += f"Valor saque: R${valor_saque: .2f}\n"
-    return Saldo, Extrato, contador
+            saldo_final_saque = saldo - valor_saque
+            saldo = saldo_final_saque
+            extrato += f"Valor saque: R${valor_saque: .2f}\n"
+    return saldo, extrato, contador
 
-def deposito(Saldo, valor_deposito, Extrato):
+def deposito(saldo, valor_deposito, extrato):
     if(valor_deposito < 0):
         print("Valor inválido!")
     else:
-        Saldo_final_deposito = valor_deposito + Saldo 
-        Saldo = Saldo_final_deposito
-        Extrato += f"Valor depósito: R${valor_deposito: .2f}\n"
-    return Saldo, Extrato
+        Saldo_final_deposito = valor_deposito + saldo 
+        saldo = Saldo_final_deposito
+        extrato += f"Valor depósito: R${valor_deposito: .2f}\n"
+    return saldo, extrato
 
-def exibir_extrato(Saldo, Extrato = Extrato):
+def exibir_extrato(saldo, extrato = extrato):
     print(f"""
 ..........EXTRATO..........""")
     
-    if(Extrato != ""):
-        print(Extrato)
+    if(extrato != ""):
+        print(extrato)
     else:
         print("Não foram realizadas movimentações.")
     
     print(f"""
-Saldo atual: R$ {Saldo:.2f}
+Saldo atual: R$ {saldo:.2f}
         
 .............................
             """)
-    return Saldo, Extrato
+    return saldo, extrato
+    
+def verifica_cpf(cpf, lista_clientes):
+    for cliente in lista_clientes:
+        if cliente["CPF"] == cpf:
+            return None
+    return lista_clientes
+    
+def cadastro_cliente(lista_clientes):
+    cpf = input("Informe o CPF do cliente: ")
+    resultado = verifica_cpf(cpf, lista_clientes)
+    if resultado == None:
+        return print("Cliente já cadastrado")
+    nome = input("Informe o nome do cliente: ")
+    data_nascimento = input("Informe a data de nascimento do cliente: ")
+    endereco = input("Informe o endereço do cliente (logradouro, número - bairro - cidade/estado): ")
+    clientes = {
+        "CPF": cpf, 
+        "Nome": nome,
+        "Data de nascimento": data_nascimento,
+        "Endereço": endereco
+    }
+    lista_clientes.append(clientes)
+    return lista_clientes
 
-def cadastro_cliente(nome, data_nascimento, cpf, logradouro, numero, bairro, cidade_estado):
-    clientes = {}
-    clientes["CPF"] = cpf
-    clientes["Nome"] = nome
-    clientes["Data de nascimento"] = data_nascimento
-    clientes["Endereço"] = {}
-    clientes["Endereço"]["Logradouro"] = logradouro
-    clientes["Endereço"]["Nº"] = numero
-    clientes["Endereço"]["Bairro"] = bairro
-    clientes["Endereço"]["Cidade/Estado"] = cidade_estado
-    return clientes
-
-def cadastro_conta(clientes, cpf, contador_conta):
-    conta = {}
-    for cliente in clientes:
-        if (cliente["CPF"] == cpf):
-            conta["CPF"] = cpf
-            conta["Nome"] = cliente["Nome"]
+def cadastro_conta(lista_clientes, contador_conta, lista_contas):
+    cpf = input("Informe o CPF do cliente: ")
+    resultado = verifica_cpf(cpf, lista_clientes)
+    for cliente in lista_clientes:
+        if resultado == None:
             contador_conta += 1
-            conta["Numero da Conta"] = contador_conta
-            conta["Agência"] = "0001"
-            print(conta)
+            conta = {
+            "CPF": cpf,
+            "Numero da Conta": contador_conta,
+            "Agência": "0001"
+            }
+            lista_contas.append(conta)
             return contador_conta
-    return print("Cliente não cadastrado")
+    return print("Cliente nao cadastrado")
 
-lista_clientes.append(cadastro_cliente("Carol", "16/12/87", "01", "Rua 01", "01", "Jardim 01", "Araraquara/SP"))
-lista_clientes.append(cadastro_cliente("Michelle", "16/11/1987", "02", "Rua 02", "02", "Jardim 02", "Araraquara/SP"))
-lista_clientes.append(cadastro_cliente("Ray", "18/12/1997", "03", "Rua 03", "03", "Jardim 03", "Araraquara/SP"))
-lista_clientes.append(cadastro_cliente("Ray", "18/12/1997", "03", "Rua 03", "03", "Jardim 03", "Araraquara/SP"))
+while True:
+    Opcao = input(menu_cadastro).upper()
 
-print(lista_clientes)
+    if(Opcao == "C"):
+        cadastro_cliente(lista_clientes)
+        
+    elif(Opcao == "IC"):
+        print(lista_clientes)
+    
+    elif(Opcao == "IO"):
+        print(lista_contas)
+    
+    elif(Opcao == "O"):
+        contador_conta = cadastro_conta(lista_clientes, contador_conta, lista_contas)
 
-contador_conta = cadastro_conta(lista_clientes, "01", contador_conta)
-contador_conta = cadastro_conta(lista_clientes, "02", contador_conta)
-contador_conta = cadastro_conta(lista_clientes, "03", contador_conta)
-contador_conta = cadastro_conta(lista_clientes, "04", contador_conta)
+    elif(Opcao == "M"):
+        print("Cadastros finalizados!")
+        break
+    
+    else:
+        print("Opção inválida")
 
 while True: 
 
-    Operacao = input(menu)
-    Operacao = Operacao.upper()
+    Operacao = input(menu_operacoes).upper()
     
     if(Operacao == "S"):
         valor_saque = float(input("Digite o valor que deseja sacar: "))
-        Saldo, Extrato, contador = saque(Saldo = Saldo, Extrato = Extrato, valor_saque = valor_saque, numero_limite_saques = LIMITE_QTD_SAQUE, valor_limite_saque = LIMITE_VALOR_SAQUE, contador = contador)
+        saldo, extrato, contador = saque(saldo = saldo, extrato = extrato, valor_saque = valor_saque, numero_limite_saques = LIMITE_QTD_SAQUE, valor_limite_saque = LIMITE_VALOR_SAQUE, contador = contador)
      
     elif(Operacao == "E"):
-        exibir_extrato(Saldo, Extrato = Extrato)
+        exibir_extrato(saldo, extrato = extrato)
 
     elif(Operacao == "D"):
         valor_deposito = float(input("Digite o valor que deseja depositar: "))
-        Saldo, Extrato = deposito(Saldo, valor_deposito, Extrato)
+        saldo, extrato = deposito(saldo, valor_deposito, extrato)
         
     elif(Operacao == "Q"):
         print("Obrigada por utilizar nossos serviços!")
