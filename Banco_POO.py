9ddbba2a5ec163bc82ec7d01c56029eaf21a9a71
Carol <carolpsa.unesp@gmail.com>
2024-06-07 18:47:57 -0300

    Correcao do acesso a variavel privado e finalizacao das classes

--- Banco_POO.py
+++ Banco_POO.py
@@ -22,6 +22,7 @@ class Conta:
 
     def depositar(self, valor):
         if valor < 0:
+            print("Valor invalido!")
             return False
         self._saldo += valor
         return True
@@ -44,12 +45,16 @@ class ContaCorrente(Conta):
         
     def sacar(self, valor):
         if valor > self._saldo:
+            print("Saldo insuficiente!")
             return False
         elif valor < 0:
+            print("Operacao nao realizada. Valor invalido!")
             return False
         elif valor > self._limite:
+            print("Operacao nao realizada. Valor de saque acima do limite!")
             return False
         elif self.contador_saques > self._limite_saques:
+            print("Operacao nao realizada. Numero de saques acima do permitido!")
             return False
         else:
             self.contador_saques += 1
@@ -77,44 +82,35 @@ class Deposito(Transacao):
         self._valor = valor
 
     def registrar(self, conta):
-        conta.self._historico = f"Deposito no valor de {self._valor:.2f}/n"
-      
+        if conta.depositar(self._valor):
+            return f"Depósito no valor de: R${self._valor:.2f}" 
+
 class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
         
     def registrar(self, conta):
-        conta.historico = f"Saque no valor de {self._valor:.2f}/n"
-
+        if conta.sacar(self._valor):
+            return f"Saque no valor de {self._valor:.2f}"
+            
 class Historico:
     def __init__(self):
         self.lista_historico = []
-        pass
 
     def adicionar_transacao(self, transacao):
-        self.lista_historico.append(transacao)
-        return print(self.lista_historico)
-
+        if transacao != None:
+            self.lista_historico.append(transacao)
+            return self.lista_historico
+       
 class Cliente:
     def __init__(self, endereco, contas):
         self._endereco = endereco
         self._contas = contas
 
     def realizar_transacao(self, conta, transacao):
-        if transacao.__class__.__name__ == "Deposito":
-            resultado = conta.depositar(transacao._valor)
-            if resultado:
-                transacao.registrar(conta)
-            else:
-                print("Nao foi possiverl realizar a operacao")
-        if transacao.__class__.__name__ ==  "Saque":
-            resultado = conta.sacar(transacao._valor)
-            if resultado:
-                transacao.registrar(conta)
-            else:
-                print("Nao foi possiverl realizar a operacao")
+        if transacao != None:
+            return print(transacao, f"\nSaldo: R$ {conta.saldo():.2f}")  
         
-
     def adicionar_conta(self, conta):
         self._contas.append(conta)
         return self._contas
@@ -127,6 +123,7 @@ class PessoaFisica(Cliente):
         self._data_nascimento = data_nascimento
 
 lista_contas1 = []
+lista_contas2 = []
 historico1 = Historico()
 historico2 = Historico()
 
@@ -136,10 +133,37 @@ pessoa1.adicionar_conta(conta1)
 conta2 = ContaCorrente(0, 2, "0001", pessoa1, historico2, 500.00, 3)
 pessoa1.adicionar_conta(conta2)
 
-deposito1 = Deposito(-1000)
-pessoa1.realizar_transacao(conta1, deposito1)
-historico1.adicionar_transacao(deposito1)
+deposito1 = Deposito(1000)
+transacao1 = deposito1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao1)
+historico1.adicionar_transacao(transacao1)
 
-saque1 = Saque(100)
-pessoa1.realizar_transacao(conta1, saque1)
-historico1.adicionar_transacao(saque1)
+saque1 = Saque(-200)
+transacao2 = saque1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao2)
+historico1.adicionar_transacao(transacao2)
+    
+saque1 = Saque(200)
+transacao3 = saque1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao3)
+historico1.adicionar_transacao(transacao3)
+
+saque1 = Saque(200)
+transacao4 = saque1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao4)
+historico1.adicionar_transacao(transacao4)
+
+saque1 = Saque(600)
+transacao5 = saque1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao5)
+historico1.adicionar_transacao(transacao5)
+
+saque1 = Saque(10)
+transacao6 = saque1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao6)
+historico1.adicionar_transacao(transacao6)
+
+deposito1 = Deposito(-1000)
+transacao7 = deposito1.registrar(conta1)
+pessoa1.realizar_transacao(conta1, transacao7)
+historico1.adicionar_transacao(transacao7)
