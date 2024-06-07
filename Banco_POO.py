785628dc774201a553fb627307775421f50fb8e5
Carol <carolpsa.unesp@gmail.com>
2024-06-06 21:24:52 -0300

    Ajuste do racional de todas as classes, exceto Historico

--- Banco_POO.py
+++ Banco_POO.py
@@ -1,36 +1,5 @@
 from abc import ABC, abstractmethod
 
-LIMITE_VALOR_SAQUE = 500.00
-LIMITE_QTD_SAQUE = 3
-contador_conta = 0
-extrato = ""
-lista_clientes=[]
-lista_contas=[]
-menu_operacoes = """ 
-    ..........MENU.OPERAÇÕES..........
-    
-    Digite S para Sacar
-    Digite D para Depósito
-    Digite E para Extrato
-    Digite Q para Sair
-    
-    .................................
-    
-    -> """
-
-menu_cadastro = """ 
-    ..........Menu.de.Cadastro..........
-    
-    Digite C para Cadastrar Cliente
-    Digite O para Cadastrar Conta
-    Digite IC para Visualizar a Lista de Clientes
-    Digite IO para Visualizar a Lista de Contas
-    Digite M para Menu de Operacoes
-    
-    ...................................
-    
-    -> """
-
 class Conta:
     def __init__(self, saldo, numero, agencia, cliente, historico):
         self._saldo = saldo
@@ -38,7 +7,7 @@ class Conta:
         self._agencia = agencia
         self._cliente = cliente
         self._historico = historico
-
+    
     def saldo(self):
         return self._saldo
 
@@ -59,11 +28,11 @@ class Conta:
 
     @classmethod
     def nova_conta(cls, cliente, numero):
-        saldo = 0
-        numero += 1
-        agencia = "0001"
-        cliente = "cliente"
-        historico = "historico"
+        cliente = cliente
+        numero = numero
+        saldo = saldo
+        agencia = agencia
+        historico = historico
         return cls(saldo, numero, agencia, cliente, historico)
 
 class ContaCorrente(Conta):
@@ -71,7 +40,7 @@ class ContaCorrente(Conta):
         super().__init__(saldo, numero, agencia, cliente, historico)
         self._limite = limite
         self._limite_saques = limite_saques
-        self.contador = 0
+        self.contador_saques = 1
         
     def sacar(self, valor):
         if valor > self._saldo:
@@ -80,42 +49,51 @@ class ContaCorrente(Conta):
             return False
         elif valor > self._limite:
             return False
-        elif self.contador >= self._limite_saques:
+        elif self.contador_saques > self._limite_saques:
             return False
         else:
-            self.contador += 1
+            self.contador_saques += 1
             self._saldo -= valor
             return True
 
-class Historico:
-    def __init__(self):
-        pass
-
-    def adicionar_transacao(self, transacao):
-        pass
-
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        cliente = cliente
+        numero = numero
+        saldo = 0
+        agencia = agencia
+        historico = historico
+        limite = limite
+        limite_saques = limite_saques
+        return cls(saldo, numero, agencia, cliente, historico, limite, limite_saques)
 
 class Transacao(ABC):
     @abstractmethod
-    def registrar(conta):
+    def registrar(self, conta):
         pass
-
 
 class Deposito(Transacao):
     def __init__(self, valor):
         self._valor = valor
 
-    def registrar(conta):
-        pass
-
-
+    def registrar(self, conta):
+        conta.self._historico = f"Deposito no valor de {self._valor:.2f}/n"
+      
 class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
         
-    def registrar(conta):
+    def registrar(self, conta):
+        conta.historico = f"Saque no valor de {self._valor:.2f}/n"
+
+class Historico:
+    def __init__(self):
+        self.lista_historico = []
         pass
 
+    def adicionar_transacao(self, transacao):
+        self.lista_historico.append(transacao)
+        return print(self.lista_historico)
 
 class Cliente:
     def __init__(self, endereco, contas):
@@ -123,28 +101,45 @@ class Cliente:
         self._contas = contas
 
     def realizar_transacao(self, conta, transacao):
-        self.conta = conta
-        self.transacao = transacao
+        if transacao.__class__.__name__ == "Deposito":
+            resultado = conta.depositar(transacao._valor)
+            if resultado:
+                transacao.registrar(conta)
+            else:
+                print("Nao foi possiverl realizar a operacao")
+        if transacao.__class__.__name__ ==  "Saque":
+            resultado = conta.sacar(transacao._valor)
+            if resultado:
+                transacao.registrar(conta)
+            else:
+                print("Nao foi possiverl realizar a operacao")
+        
 
     def adicionar_conta(self, conta):
-        self.conta = conta
-
+        self._contas.append(conta)
+        return self._contas
 
 class PessoaFisica(Cliente):
     def __init__(self, endereco, contas, cpf, nome, data_nascimento):
-        super().__init__(endereco, contas, cpf, nome, data_nascimento)
+        super().__init__(endereco, contas)
         self._cpf= cpf
         self._nome = nome
         self._data_nascimento = data_nascimento
 
-    def adicionar_conta(self, conta):
-        self.conta = conta
+lista_contas1 = []
+historico1 = Historico()
+historico2 = Historico()
 
-conta1 = ContaCorrente(0.00, 1, "0001", "cliente", "historico", 500.00, 3)
-print(conta1._saldo)
-print(conta1.depositar(1000.00))
-print(conta1.sacar(100.00))
-print(conta1._saldo)
-print(conta1.sacar(100.00))
-print(conta1.sacar(100.00))
-print(conta1.sacar(100.00))
+pessoa1 = PessoaFisica("Rua 01", lista_contas1, "01", "Carol", "16/12")
+conta1 = ContaCorrente(0, 1, "0001", pessoa1, historico1, 500.00, 3)
+pessoa1.adicionar_conta(conta1)
+conta2 = ContaCorrente(0, 2, "0001", pessoa1, historico2, 500.00, 3)
+pessoa1.adicionar_conta(conta2)
+
+deposito1 = Deposito(-1000)
+pessoa1.realizar_transacao(conta1, deposito1)
+historico1.adicionar_transacao(deposito1)
+
+saque1 = Saque(100)
+pessoa1.realizar_transacao(conta1, saque1)
+historico1.adicionar_transacao(saque1)
